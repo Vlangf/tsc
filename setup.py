c64a76bed70831b218cd9177a068c4dca536e296
vlangf <94Afiber>
2022-06-20 18:48:38 +0300

    firs commit

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from tsc import __version__
 
 setup(
     name='tsc',
     version=__version__,
-
+    packages=find_packages(),
     url='https://github.com/Vlangf/tsc',
-    author='Vlangf',
-
-    py_modules=['tsc'],
+    author='Vlangf'
 )
